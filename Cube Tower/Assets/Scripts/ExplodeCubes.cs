f86d4ae6caa8263ce2f8ab14e36683b92f1e21d1
Gaspit <ardelyan.s@gmail.com>
2023-03-15 15:45:00 +0200

    adding lose logic

--- Cube Tower/Assets/Scripts/ExplodeCubes.cs
+++ Cube Tower/Assets/Scripts/ExplodeCubes.cs
@@ -1,22 +1,3 @@
-using UnityEngine;
-
-public class ExplodeCubes : MonoBehaviour
-{
-    private bool _collisionSet;
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Cube") && !_collisionSet)
-        {
-            for (int i = collision.transform.childCount - 1; i >= 0; i--)
-            {
-                Transform child = collision.transform.GetChild(i);
-                child.gameObject.AddComponent<Rigidbody>();
-                child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f);
-                child.SetParent(null);
-            }
-            
-            Destroy(collision.gameObject);
-            _collisionSet = true;
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:586bf421ad46b4ed856dbe709e1951d64e399ea49688a6305802aa358a55fd10
+size 862
