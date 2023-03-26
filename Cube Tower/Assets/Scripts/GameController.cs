b260c414353711212d1244c91b59fa22b9b648e7
Gaspit <ardelyan.s@gmail.com>
2023-03-26 23:47:31 +0300

    inportant

--- Cube Tower/Assets/Scripts/GameController.cs
+++ Cube Tower/Assets/Scripts/GameController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eda7acca2ddfee9fcf66e2ff60e873b34c271ba32d57b2b3d004504207f14e09
-size 9166
+oid sha256:1c19b318500ca08f0b5acf2fe57fba8663e16fd29cead0f5c5d65b6efce7bd07
+size 9110
